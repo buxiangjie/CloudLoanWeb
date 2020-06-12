5893cd8a0279b0ddca538130af8da20783869bc4
buxiangjie <bxj3416162@163.com>
2020-06-12 16:51:47 +0800

    base
    Former-commit-id: 6cbb94b905d29e1844cc22a5d9593f64d1e665a7

--- common/base.py
+++ common/base.py
@@ -6,3 +6,30 @@
 """
 
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class Bash:
+
+	def __init__(self, driver):
+		self.driver = webdriver.Chrome()
+
+	def __open(self, url):
+		self.driver.get(url)
+
+	def open(self, url):
+		self.__open(url)
+
+	def find_element(self, *loc, time=20):
+		try:
+			WebDriverWait(self.driver, time).until(self.driver.find_element(loc))
+			return self.find_element(loc)
+		except Exception as e:
+			raise str(e)
+
+	def quit(self):
+		self.quit()
+
+	def close(self):
+		self.close()
+
