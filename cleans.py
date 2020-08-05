c734a516f99b5970c0f47153314665026fe66bf4
buxiangjie <bxj3416162@163.com>
2020-08-06 01:19:03 +0800

    update
    Former-commit-id: 1bd67d197e9a31e36f8337c17a097dc54b08a1aa

--- cleans.py
+++ cleans.py
@@ -1,9 +1,14 @@
 import os
 import shutil
 
-if os.path.exists("./report"):
-	shutil.rmtree("./report")
-if os.path.exists("./screenshot"):
-	shutil.rmtree("./screenshot")
-if os.path.exists("./results"):
-	shutil.rmtree("./results")
+
+def cleans():
+	os.system("allure generate --clean ./results -o ./report")
+	if os.path.exists("./results"):
+		shutil.rmtree("./results")
+	if os.path.exists("./screenshot"):
+		shutil.rmtree("./screenshot")
+
+
+if __name__ == '__main__':
+	cleans()
--- pages/join_us.py
+++ pages/join_us.py
@@ -5,6 +5,8 @@
 @describe: 
 """
 
+import allure
+
 from selenium.webdriver.common.by import By
 from common.base import Base
 
@@ -12,5 +14,6 @@ from common.base import Base
 class JoinUs(Base):
 	recruitment = (By.LINK_TEXT, "人才招聘")
 
+	@allure.step("检查人才招聘页面是否跳转成功")
 	def check_join_us(self):
 		assert self.find_element(*self.recruitment)
