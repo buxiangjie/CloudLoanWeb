ec34c9c266d2966c56de7816829284f1693f0e4f
buxiangjie <bxj3416162@163.com>
2020-08-05 15:41:06 +0800

    update conftest.py
    Former-commit-id: 862a03c9864680a854516bf4c0e1e1fe4002014b

---
+++ cases/conftest.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+"""
+@auth:buxiangjie
+@date:2020-05-12 11:26:00
+@describe: 
+"""
+
+import pytest
+
+
+def pytest_html_report_title(report):
+	report.title = "CloudLoan Test Report"
