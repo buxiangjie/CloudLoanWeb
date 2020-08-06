342f97fdac83a12b0ab48c4d27a40a1f07fb03db
buxiangjie <bxj3416162@163.com>
2020-08-06 23:59:46 +0800

    2

--- cases/conftest.py
+++ cases/conftest.py
@@ -87,6 +87,7 @@ def drivers(request):
 	chrome_options = Options()
 	chrome_options.add_argument('--headless')
 	chrome_options.add_argument('--no-sandbox')
+	chrome_options.binary_location = r"F:\Program Files\chromedriver.exe"
 	chrome_options.add_argument('--window-size=1920,1080')
 	driver = webdriver.Chrome(options=chrome_options)
 	print(f"浏览器{driver}")
