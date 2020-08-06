dd2c14e448af0f26c6a6dc0953c95ff456d31732
buxiangjie <bxj3416162@163.com>
2020-08-06 14:00:58 +0800

    update conftest.py
    Former-commit-id: b5dfd733a34decab669bb3d5e2e2996813fda56f

--- cleans.py
+++ cleans.py
@@ -3,12 +3,13 @@ import shutil
 
 
 def cleans():
-	os.system("allure generate --clean ./results -o ./report")
-	if os.path.exists("./results"):
-		shutil.rmtree("./results")
-	if os.path.exists("./screenshot"):
-		shutil.rmtree("./screenshot")
-
+	try:
+		os.system("allure generate --clean ./results -o ./report")
+	finally:
+		if os.path.exists("./results"):
+			shutil.rmtree("./results")
+		if os.path.exists("./screenshot"):
+			shutil.rmtree("./screenshot")
 
 if __name__ == '__main__':
 	cleans()
